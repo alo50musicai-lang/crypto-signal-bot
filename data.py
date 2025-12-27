35b6251401b71c0bd446dfa1a6b8db3a3f1eb658
alo50musicai-lang <alo50musicai@gmail.com>
2025-12-27 18:49:49 +0330

    Create data.py

---
+++ data.py
@@ -0,0 +1,22 @@
+import requests
+
+def get_klines(symbol="BTCUSDT", interval="5m", limit=200):
+    url = "https://api.binance.com/api/v3/klines"
+    params = {
+        "symbol": symbol,
+        "interval": interval,
+        "limit": limit
+    }
+    r = requests.get(url, params=params, timeout=10)
+    data = r.json()
+
+    candles = []
+    for k in data:
+        candles.append({
+            "open": float(k[1]),
+            "high": float(k[2]),
+            "low": float(k[3]),
+            "close": float(k[4]),
+            "volume": float(k[5])
+        })
+    return candles
